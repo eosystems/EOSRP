eac6e574cb20075296727be9e53e4103acbbd48e
Shinsuke Nishio <nishio@densan-labs.net>
2016-09-23 02:47:33 +0900

    補償額設定一覧と編集画面作成 (編集画面は作成途中)

---
+++ frontend/src/app/guarantee/edit-guarantee/edit-guarantee.component.ts
@@ -0,0 +1,49 @@
+import {Component, Input} from '@angular/core';
+import {Guarantee} from '../../models/guarantee';
+
+@Component({
+  selector: 'edit-guarantee',
+  templateUrl: "./edit-guarantee.template.html"
+})
+
+export class EditGuaranteeComponent {
+  @Input() guarantees: Array<Guarantee>;
+
+  colHeaders:Array<string> = [
+    'Id', 'ShipId', 'ShipType', 'ShipName', 'Price', 'Description'
+  ];
+  columns:Array<any> = [
+    {
+      data: 'id',
+      readOnly: true
+    },
+    {
+      data: 'ship_id',
+      readOnly: true
+    },
+    {
+      data: 'guarantee_ship_type',
+      readOnly: true
+    },
+    {
+      data: 'guarantee_ship_name',
+      readOnly: true
+    },
+    {
+      data: 'price',
+      type: 'numeric',
+      format: '0,0.00'
+    },
+    {
+      data: 'description'
+    },
+  ];
+  options: any = {
+    stretchH: 'all',
+    columnSorting: true
+  };
+
+  onEditRecord(event: any) {
+    console.log(event);
+  }
+}
