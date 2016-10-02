3ec39397c426583f02582a266b91a99daeb8846d
Shinsuke Nishio <nishio@densan-labs.net>
2016-10-02 23:38:57 +0900

    編集結果をPOSTする処理作成

--- frontend/src/app/guarantee/guarantee.service.ts
+++ frontend/src/app/guarantee/guarantee.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {GuaranteeTypeForm} from '../models/guarantee-type-form';
-import {Observable} from 'rxjs';
 import {JsonObjectMapper} from '../common/json-property/json-object-mapper';
+import {Guarantee} from '../models/guarantee';
 
 @Injectable()
 export class GuaranteeService {
@@ -18,5 +18,22 @@ export class GuaranteeService {
         let form = JsonObjectMapper.deserialize(GuaranteeTypeForm, r);
         return form;
       });
+  }
+
+  bulkUpdate(guarantees: Array<Guarantee>): any {
+    let values = guarantees.map(g => {
+      return {
+        id: g.id,
+        price: g.price,
+        description: g.description
+      };
+    })
+    let body = JSON.stringify({
+      guarantees: values
+    });
+
+    return this
+      ._http
+      .put(process.env.API_URL + '/api/guarantees/update_all', body);
   }
 }
