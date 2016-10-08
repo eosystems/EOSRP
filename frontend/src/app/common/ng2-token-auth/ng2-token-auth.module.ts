b000e6a6e0f5684064138d174c062d870b702dcb
Shinsuke Nishio <nishio@densan-labs.net>
2016-10-09 00:29:51 +0900

    認証用モジュール追加

---
+++ frontend/src/app/common/ng2-token-auth/ng2-token-auth.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core/src/metadata/ng_module';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+  ],
+  exports: [
+  ],
+  providers: [
+  ]
+})
+
+export class Ng2TokenAuthModule {}
