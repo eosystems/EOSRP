cf52bd1da1757444d272741a0a7acacac8474b8f
Shinsuke Nishio <nishio@densan-labs.net>
2016-10-09 04:22:30 +0900

    Navbarからサインアウトできるように & ユーザ情報表示

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ng2TokenAuthService} from '../common/ng2-token-auth/ng2-token-auth.service';
 
 @Component({
   selector: 'navbar',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 
 export class NavBar {
+  constructor(
+    private auth: Ng2TokenAuthService
+  ) { }
+
+  signOut() {
+    this.auth.signOut();
+  }
+
+  userName(): string {
+    let u = this.auth.getUserData();
+    return u['name'] || 'UID: ' + u['uid'];
+  }
 }
