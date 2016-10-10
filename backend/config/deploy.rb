41fc260802d1e370a4f7f699a7df870d5d1d073c
r_nishio <quick.xp@me.com>
2016-10-10 18:19:05 +0900

    subdirectory 関連各種修正

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -36,7 +36,7 @@ set :pty, true
 
 # Shared
 set :linked_files, fetch(:linked_files, []).push('backend/config/database.yml','backend/config/secrets.yml','backend/config/settings.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log','tmp/pids','tmp/cache','tmp/sockets','vendor/bundle','public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('backend/log','backend/tmp/pids','backend/tmp/cache','backend/tmp/sockets','backend/vendor/bundle','backend/public/system')
 #set :linked_files, %w{config/database.yml config/secrets.yml config/settings.yml}
 #set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
