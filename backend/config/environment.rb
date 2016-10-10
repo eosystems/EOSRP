a9a51478a0443d31b4b0c05129c28d17c0493c70
r_nishio <quick.xp@me.com>
2016-10-10 16:30:35 +0900

    log format

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -3,3 +3,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+class Logger
+  class Formatter
+    def call(severity, time, progname, msg)
+      if msg.class.to_s == 'String'
+        msg = msg.gsub(/\n/, '')
+        if msg.present? && !((msg.include?('assets') || msg.include?('erb')) && severity == 'ERROR')
+          format = "[%s %d] %5s -- %s: \'%s\'\n"
+          format % ["#{time.strftime('%Y-%m-%dT%H:%M:%S')}.#{'%06d' % time.usec.to_s}", $$, severity, progname, msg2str(msg)]
+        end
+      end
+    end
+  end
+end
