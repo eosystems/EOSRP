ea3c0b3f2df16697ed18e563b2197c624af77933
r.nishio <quick.xp@gmail.com>
2018-06-17 21:38:24 +0900

    互換

--- backend/app/controllers/api/zkills_controller.rb
+++ backend/app/controllers/api/zkills_controller.rb
@@ -6,6 +6,6 @@ class Api::ZkillsController < Api::ApiController
     client = ZkillClient.new
     @zkill = client.fetch_loss(client.get_kill_id_from_url(url))
     @zkill_old = {}
-    @zkill_old["victim"] = { characterID: @zkill.victim.character_id, characterName: 'Not Support sorry..'}
+    @zkill_old["victim"] = { characterID: @zkill.victim.character_id, characterName: 'Not Support sorry..', shipTypeID: @zkill.victim.ship_type_id}
   end
 end
--- backend/app/views/api/zkills/index.json.jbuilder
+++ backend/app/views/api/zkills/index.json.jbuilder
@@ -3,7 +3,7 @@ json.per 1
 json.totalCount 1
 
 json.results do
-  json.(@zkill.victim, :characterID, :characterName, :shipTypeID)
+  json.(@zkill_old.victim, :characterID, :characterName, :shipTypeID)
   json.(@zkill.zkb, :totalValue)
   json.shipName Ship.find(@zkill.victim.ship_type_id).ship_name
   json.killTime @zkill.killmail_time
