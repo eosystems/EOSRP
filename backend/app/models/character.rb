997bd88ad3b27d28199d9bd11e2f09793557511b
r.nishio <quick.xp@gmail.com>
2018-06-17 21:10:00 +0900

    ESI対応

--- backend/app/models/character.rb
+++ backend/app/models/character.rb
@@ -1,7 +1,7 @@
 class Character
   include ActiveModel::Model
 
-  EVE_CHAR_API_PATH = "https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=%d".freeze
+  EVE_CHAR_API_PATH = "https://esi.evetech.net/latest/characters/%d/".freeze
 
   attr_accessor *%i(
     character_id character_name corporation_id corporation_name alliance_id alliance_name
@@ -10,14 +10,12 @@ class Character
 
   def self.initialized_by(token, account_id)
     account = token.get(EVE_CHAR_API_PATH % account_id)
-    result = Hash.from_xml(account.body)["eveapi"]["result"]
+    result = JSON.parse(account.body)
     Character.new.tap do |c|
-      c.character_id = result["characterID"]
-      c.character_name = result["characterName"]
-      c.corporation_id = result["corporationID"] if result["corporationID"].present?
-      c.corporation_name = result["corporation"] if result["corporation"].present?
-      c.alliance_id = result["allianceID"] if result["allianceID"].present?
-      c.alliance_name = result["alliance"] if result["alliance"].present?
+      c.character_id = account_id
+      c.character_name = result["name"]
+      c.corporation_id = result["corporation_id"] if result["corporation_id"].present?
+      c.alliance_id = result["alliance_id"] if result["alliance_id"].present?
     end
   end
 end
